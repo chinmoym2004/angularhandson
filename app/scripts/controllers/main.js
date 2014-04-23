eb785246937d3d63f4217e223994898c92e51f64
shareonline <chinmoym2004@gmail.com>
2014-04-23 11:43:17 +0530

    angular_laravel via proxy

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('angularApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope , $resource) {
+    //$resource('/api').query();
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
